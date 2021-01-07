791d5f56ba82473ed81f00752d795ec9fc84970d
james-chun <chc3484@gmail.com>
2021-01-07 17:29:23 +0900

    test

---
+++ DemoApp1/DemoApp1/DemoApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp1App.swift
+//  DemoApp1
+//
+//  Created by gamepub on 2021/01/07.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SDK1/SDK1/SDK1.h
@@ -0,0 +1,18 @@
+//
+//  SDK1.h
+//  SDK1
+//
+//  Created by gamepub on 2021/01/07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDK1.
+FOUNDATION_EXPORT double SDK1VersionNumber;
+
+//! Project version string for SDK1.
+FOUNDATION_EXPORT const unsigned char SDK1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDK1/PublicHeader.h>
+
+
